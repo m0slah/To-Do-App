51e6dd603820e3dcbe4904e9d153aceb27dd3451
m0slah <muhammedslahqadir@gmail.com>
2023-08-06 12:14:01 +0300

    complate template structure

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template c>
   <div class="text-center min-h-screen my-10">
-  <h1 class="text-white font-bold text-xl my-5">My To Do List</h1>
-    <ul class="border-2 bg-blue-200 mx-auto w-1/2 rounded-lg border-0">
+    <h1 class="text-white font-bold text-xl my-5">My To Do List</h1>
+    <input
+      class="p-2 rounded-sm my-2 outline-none"
+      v-model="newTask"
+      placeholder="Add a new task"
+    />
+
+    <ul class="bg-gray-500 mx-auto w-1/2 rounded-lg border-0">
       <li
         v-for="(task, key) in tasks"
         :key="key"
-        class="p-4 text-center"
+        class="p-4 flex justify-between text-white text-lg capitalize"
       >
         {{ task }}
+        <button onclick="#" class="justify-end text-red-500">Remove</button>
       </li>
     </ul>
   </div>
